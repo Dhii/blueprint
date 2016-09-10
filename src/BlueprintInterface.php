2cdae5e44abc323cee0aac92e86651eb2b50a64c
Miguel Muscat <miguelmuscat93@gmail.com>
2016-09-10 17:52:09 +0200

    Fixed code standards

--- src/BlueprintInterface.php
+++ src/BlueprintInterface.php
@@ -3,23 +3,21 @@
 namespace Dhii\Blueprint;
 
 /**
- * A blueprint represents a set of data that defines how an object should be built.
- * 
+ * An object that can serve as a blueprint for a buildable object.
+ *
  * Blueprints should differ from one instance to another, allowing different instances of the same blueprint class to
  * define different objects. At the same time, instances considered to be equal must define the same object to be
  * built.
- * 
+ *
  * @author Miguel Muscat <miguelmuscat93@gmail.com>
  */
 interface BlueprintInterface
 {
-
     /**
      * Gets the blueprint's build data.
-     * 
+     *
      * This data should represent the object to be built. Instances considered to be equal should return the same data
      * set whereas instances considered to be not equal must return different data sets.
-     * 
      *
      * @return array An array containing the build data.
      */
--- src/BuilderInterface.php
+++ src/BuilderInterface.php
@@ -4,15 +4,14 @@ namespace Dhii\Blueprint;
 
 /**
  * An object that can use blueprint instances to build objects.
- * 
+ *
  * @author Miguel Muscat <miguelmuscat93@gmail.com>
  */
 interface BuilderInterface
 {
-
     /**
      * Builds an object based on the given blueprint instance.
-     * 
+     *
      * @param BlueprintInterface $blueprint The blueprint to use for building.
      *
      * @throws \Dhii\Bluepriint\UnsupportedBlueprintException If the builder does not support the given blueprint.
