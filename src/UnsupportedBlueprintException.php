7f263df37141f7597e0146ed84a67bd4f67ca178
Miguel Muscat <miguelmuscat93@gmail.com>
2016-09-10 17:47:21 +0200

    Added `UnsupportedBlueprintException` class

---
+++ src/UnsupportedBlueprintException.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Dhii\Bluepriint;
+
+/**
+ * Exception class used when a builder is given a blueprint that it does not suport.
+ *
+ * @author Miguel Muscat <miguelmuscat93@gmail.com>
+ */
+class UnsupportedBlueprintException extends \Exception
+{
+    /**
+     * {@inheritdoc}
+     *
+     * @param \Dhii\Bluepriint\BuilderInterface   $builder   The builder instance.
+     * @param \Dhii\Bluepriint\BlueprintInterface $blueprint The blueprint instance.
+     * @param int                                 $code      The exception code.
+     * @param \Exception                          $previous  The previous exception in the chain.
+     */
+    public function __construct(BuilderInterface $builder, BlueprintInterface $blueprint, $code = 0, \Exception $previous = null)
+    {
+        $message = sprintf('Builder %s does not support blueprints with build type "%s".',
+            get_class($builder),
+            $blueprint->getBuildType()
+        );
+        parent::__construct($message, $code, $previous);
+    }
+}
